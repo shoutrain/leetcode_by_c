6819135f9a64e718280021722241bc7ce7aeafc0
rafael <shoutrain.goo@gmail.com>
2015-12-25 10:08:32 +0800

    finished permutation sequence

--- src/permutation_sequence.c
+++ src/permutation_sequence.c
@@ -8,25 +8,38 @@
 #include "permutation_sequence.h"
 
 char *getPermutation(int n, int k) {
+	if (1 == n) {
+		char *result = (char *)malloc(2 * sizeof(char));
+
+		result[0] = '1';
+		result[1] = '\0';
+
+		return result;
+	}
+
+	k--;
+
 	int i;
 	int m = n - 1;
 	int *sums = (int *) malloc(m * sizeof(int));
 	int *nums = (int *) malloc(n * sizeof(int));
 
-	for (i = 0; i < m; i++) {
-		if (0 == i) {
-			sums[i] = i + 1;
+	for (i = 1; i <= m; i++) {
+		if (1 == i) {
+			sums[i - 1] = i;
 		} else {
-			sums[i] = sums[i - 1] * (i + 1);
+			sums[i - 1] = sums[i - 2] * i;
 		}
 
-		nums[i] = i + 1;
+		nums[i - 1] = i;
 	}
 
-	nums[n - 1] = n;
+	nums[m] = n;
 
 	int *mods = (int *) malloc(m * sizeof(int));
-	char *locs = (char *) malloc(n * sizeof(char));
+	char *locs = (char *) malloc((n + 1) * sizeof(char));
+
+	locs[n] = '\0';
 
 	for (i = 0; i < n; i++) {
 		int j = m - i - 1;
@@ -43,15 +56,19 @@ char *getPermutation(int n, int k) {
 			}
 		}
 
-		j = 0;
 		int l = 0;
 
-		while (l < locs[i]) {
-			if (nums[j++] != 0) {
+		for (j = 0; j < n; j++) {
+			if (0 != nums[j]) {
+				if (l == locs[i]) {
+					break;
+				}
+
 				l++;
 			}
 		}
 
+		assert(j < n);
 		locs[i] = nums[j] + 48;
 		nums[j] = 0;
 	}
@@ -59,8 +76,8 @@ char *getPermutation(int n, int k) {
 	return locs;
 }
 
-#define N 5
-#define K 10
+#define N 1
+#define K 1
 
 static void _run() {
 	printf("The %dth of ermutation of %d is %s\n", K, N, getPermutation(N, K));
